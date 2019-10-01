07c9cefa9abdfcb6b0ff5060aaf3d63395b9e79a
Francisco Garrido Mosqueira <garridof@ryanair.com>
2019-10-01 23:31:57 +0200

    Included actuator

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-java -jar -Dspring.profiles.active=dev /home/ec2-user/awsdemo-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
+#java -jar -Dspring.profiles.active=dev /home/ec2-user/awsdemo-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
+java -jar awsdemo-0.0.1-SNAPSHOT.jar
