35644bbacc9c705102d3f5dc86106086630e5d64
Francisco Garrido Mosqueira <garridof@ryanair.com>
2019-10-02 10:18:52 +0200

    Included Json

--- src/main/java/com/fran/demo/app/EC2Info.java
+++ src/main/java/com/fran/demo/app/EC2Info.java
@@ -1,5 +1,6 @@
 package com.fran.demo.app;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -10,11 +11,12 @@ import static java.lang.String.join;
 @RequiredArgsConstructor
 @Builder
 @Getter
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class EC2Info {
-    private final String InstanceId = EC2MetadataUtils.getInstanceId();
-    private final String EC2InstanceRegion = EC2MetadataUtils.getEC2InstanceRegion();
-    private final String AvailabilityZone = EC2MetadataUtils.getAvailabilityZone();
-    private final String SecurityGroups = join(",", EC2MetadataUtils.getSecurityGroups());
-    private final String UserData = EC2MetadataUtils.getUserData();
-    private final String LocalHostName = EC2MetadataUtils.getLocalHostName();
+    private final String instanceId = EC2MetadataUtils.getInstanceId();
+    private final String ec2InstanceRegion = EC2MetadataUtils.getEC2InstanceRegion();
+    private final String availabilityZone = EC2MetadataUtils.getAvailabilityZone();
+    private final String securityGroups = join(",", EC2MetadataUtils.getSecurityGroups());
+    private final String userData = EC2MetadataUtils.getUserData();
+    private final String localHostName = EC2MetadataUtils.getLocalHostName();
 }
